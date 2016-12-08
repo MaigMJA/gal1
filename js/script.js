67a20cfefc5ba193c0d88f52d87aca1a88fdf346
DESKTOP-O408SC9\maign <maignan.marc@gmail.com>
2016-12-08 07:46:04 +0100

    add

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Pop portrait
+document.getElementsByClassName('pop')[0].addEventListener("click", function() {
+if(this.className === 'pop grow') {
+  this.className = 'pop';
+} else {
+  this.className = 'pop grow';
+}
+});
+//fin pop portrait
+
+
+
+$("#full_screen,#bandeau").click(function(){
+	$("#bandeau").toggle();
+});
